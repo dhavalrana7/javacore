9f62dcd1a90f1198c7ec17744ead3bf7d911fa37
dhavalrana7 <dhaval_rana@yahoo.com>
2015-12-19 20:58:11 -0600

    Adding Two Numbers
    Adding Two Numbers

---
+++ ArithmeticOperation/src/com/digitek/mathoperators/MathOprations.java
@@ -0,0 +1,17 @@
+package com.digitek.mathoperators;
+
+public class MathOprations {
+	public static void addNumbers () {
+		int a = 15;
+		int b = 25;
+		int c = a+b ;
+		System.out.println("result of adding two numbers is :" +c);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		addNumbers();
+	}
+
+}
