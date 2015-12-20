c2fed6d684ed8d46009a3e557e6c4169da9d83d1
dhavalrana7 <dhaval_rana@yahoo.com>
2015-12-20 10:20:16 -0600

    Using only Main Class
    Using only main class

---
+++ NewArithmatic/src/com/digitek/mathoperators1/NewArithmatic.java
@@ -0,0 +1,23 @@
+package com.digitek.mathoperators1;
+
+public class NewArithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            int i = 10;
+            int j = 30;
+            double k = 10.5;
+            double l = 20.5;
+            
+            // Adding Two Numbers
+            System.out.println ( " Adding i + j is :  " + (i+j));
+            
+            // Substracting two Numbers
+            
+            System.out.println ( " Substrating j - i is :  " + (j - i));
+
+            
+            
+	}
+
+}
