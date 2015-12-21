282120b807cdb8de61f440d8115b20cd3e8e6137
dhavalrana7 <dhaval_rana@yahoo.com>
2015-12-21 17:34:47 -0600

    Example to find out ODD EVEN Numbers
    Basic way to find out ODD EVEN.

---
+++ ExOddEven/src/ExOddEvenFor.java
@@ -0,0 +1,19 @@
+
+public class ExOddEvenFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for ( int i=20; i>0; i--) {
+			System.out.print("    Even Value of i is " + i  );
+			i--;
+			System.out.print("      Odd Value of i is "     +i);
+			System.out.println();
+			
+		}
+
+	}
+
+
+	}
+
+
